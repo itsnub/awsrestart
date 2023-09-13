dfc5922652d31e97fd97c47d1bdcd097098058aa
itsnub <girianuj13@gmail.com>
2023-09-13 13:56:57 +0545

    lab 6 files

---
+++ numeric-data.py
@@ -0,0 +1,30 @@
+print("Python has three numeric types:int, float, and complex")
+
+myValue=1
+print(myValue)
+print(type(myValue))
+
+print(str(myValue)+" is of the data type "+str(type(myValue)))
+
+
+myvalue=3.14
+print(myValue)
+print(type(myValue))
+print(str(myValue)+ " is of the data type "+str(type(myValue)))
+
+
+myValue=5j
+print(myValue)
+print(type(myValue))
+print(str(myValue)+" is of the data type "+str(type(myValue)))
+
+
+myValue=True
+print(myValue)
+print(type(myValue))
+print(str(myValue)+" is the data type "+str(type(myValue)))
+
+myvalue=False
+print(myValue)
+print(type(myValue))
+print(str(myValue)+ " is of the data type "+str(type(myValue)))
